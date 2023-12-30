72e2de5f0f218b7e68a206e57668d04caeec59ca
amiransari27 <amir_ansari50@yahoo.in>
2023-12-30 11:43:59 +0530

    Update powerOfTwo.go

--- problems/powerOfTwo.go
+++ problems/powerOfTwo.go
@@ -1,18 +1,26 @@
 package problems
 
-import "math"
-
-func IsPowerOfTwo(n float64) bool {
+func IsPowerOfTwo(n int) bool {
 	if n < 1 {
 		return false
 	}
 
 	for n > 1 {
-		if math.Mod(n, 2) != 0 {
+		if n%2 != 0 {
 			return false
 		}
 		n = n / 2
 	}
 
 	return true
+}
+
+// if n is power of two
+// when we run a bit and operation between n&(n-1) it always return 0
+func IsPowerOfTwoBitWise(n int) bool {
+	if n < 1 {
+		return false
+	}
+
+	return (n & (n - 1)) == 0
 }
