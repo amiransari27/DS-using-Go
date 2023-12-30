b0503e4697d9ccf991f828871360c71486b8483e
amiransari27 <amir_ansari50@yahoo.in>
2023-12-30 15:20:37 +0530

    Update factorial.go

--- factorial/factorial.go
+++ factorial/factorial.go
@@ -1,3 +1,5 @@
+package main
+
 import "fmt"
 
 func factorial(n int) int {
@@ -10,9 +12,22 @@ func factorial(n int) int {
 	return fact
 }
 
+func factorialRec(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorialRec(n-1)
+}
+
 func main() {
 	fmt.Println(factorial(0))
 	fmt.Println(factorial(2))
 	fmt.Println(factorial(4))
 	fmt.Println(factorial(5))
+	fmt.Println("-----")
+
+	fmt.Println(factorialRec(0))
+	fmt.Println(factorialRec(2))
+	fmt.Println(factorialRec(4))
+	fmt.Println(factorialRec(5))
 }
