9fb458a9855f3c6998e66bad738bf242cdc2984a
amiransari27 <amir_ansari50@yahoo.in>
2024-01-01 17:10:10 +0530

    Create sort.go - bubble sort

---
+++ problems/sort.go
@@ -0,0 +1,40 @@
+package problems
+
+func BubbleSort(arr []int) []int {
+
+	for i := 0; i < len(arr); i++ {
+		right := len(arr) - 1
+		left := right - 1
+
+		// for left > -1 {
+		for (left - i) > -1 {
+			if arr[left] > arr[right] {
+				tmp := arr[left]
+				arr[left] = arr[right]
+				arr[right] = tmp
+			}
+			right--
+			left--
+		}
+
+	}
+
+	return arr
+}
+
+func BubbleSort2(arr []int) []int {
+
+	for swapped := true; swapped; { // this is how we can achieve do while functionality in golang
+		swapped = false
+		for i := 0; i < len(arr)-1; i++ {
+			if arr[i] > arr[i+1] {
+				tmp := arr[i]
+				arr[i] = arr[i+1]
+				arr[i+1] = tmp
+				swapped = true
+			}
+		}
+	}
+
+	return arr
+}
