0500302ea0ce8e69fdea3de0f0b826c02e8aca72
amiransari27 <amir_ansari50@yahoo.in>
2024-01-11 12:14:38 +0530

    Create zigzag.go

---
+++ leetcode/zigzag.go
@@ -0,0 +1,36 @@
+package leetcode
+
+func Convert(s string, numRows int) string {
+	if numRows < 2 {
+		return s
+	}
+
+	var result string = ""
+
+	for i := 0; i < numRows; i++ {
+		idx := i
+		driveDown := 2 * (numRows - 1 - i)
+		driveUp := 2 * i
+		driveToggle := true
+
+		for idx < len(s) {
+			result += string(s[idx])
+
+			if i == 0 {
+				idx += driveDown
+			} else if i == numRows-1 {
+				idx += driveUp
+			} else {
+				if driveToggle {
+					idx += driveDown
+				} else {
+					idx += driveUp
+				}
+				driveToggle = !driveToggle
+			}
+
+		}
+	}
+	return result
+
+}
