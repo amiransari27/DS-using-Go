4166e1572a349eea19adfce04b95719249ec2818
Amir Ansari <amir_ansari50@yahoo.in>
2024-02-04 11:02:56 +0530

    Create maxOperations.go

---
+++ leetcode75/maxOperations.go
@@ -0,0 +1,22 @@
+package leetcode
+
+import "slices"
+
+func MaxOperations(nums []int, k int) int {
+	slices.Sort(nums)
+
+	var low, high = 0, len(nums) - 1
+	count := 0
+	for low < high {
+		if nums[low]+nums[high] == k {
+			count++
+			low++
+			high--
+		} else if nums[low]+nums[high] < k {
+			low++
+		} else {
+			high--
+		}
+	}
+	return count
+}
