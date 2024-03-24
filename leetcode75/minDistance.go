33f0790c4889245547e92da5debbed7c13ff8068
Amir Ansari <amir_ansari50@yahoo.in>
2024-03-24 14:13:20 +0530

    Update minDistance.go

--- leetcode75/minDistance.go
+++ leetcode75/minDistance.go
@@ -1,9 +1,21 @@
-func minDistance(word1 string, word2 string) int {
+package leetcode75
 
-	return solve_minDistance(word1, word2, len(word1), len(word2))
+func MinDistance(word1 string, word2 string) int {
+	m, n := len(word1), len(word2)
+
+	memo := make([][]int, m+1)
+	for i := range memo {
+		tmp := make([]int, n+1)
+		for j := range tmp {
+			tmp[j] = -1
+		}
+		memo[i] = tmp
+	}
+
+	return solve_minDistance(memo, word1, word2, m, n)
 }
 
-func solve_minDistance(word1 string, word2 string, i int, j int) int {
+func solve_minDistance(memo [][]int, word1 string, word2 string, i int, j int) int {
 
 	if i == 0 {
 		return j
@@ -13,12 +25,18 @@ func solve_minDistance(word1 string, word2 string, i int, j int) int {
 		return i
 	}
 
-	if word1[i-1] == word2[j-1] {
-		return solve_minDistance(word1, word2, i-1, j-1)
+	if memo[i][j] != -1 {
+		return memo[i][j]
 	}
-	insert := 1 + solve_minDistance(word1, word2, i, j-1)
-	delete := 1 + solve_minDistance(word1, word2, i-1, j)
-	replace := 1 + solve_minDistance(word1, word2, i-1, j-1)
 
-	return min(insert, delete, replace)
+	if word1[i-1] == word2[j-1] {
+		return solve_minDistance(memo, word1, word2, i-1, j-1)
+	}
+	insert := 1 + solve_minDistance(memo, word1, word2, i, j-1)
+	delete := 1 + solve_minDistance(memo, word1, word2, i-1, j)
+	replace := 1 + solve_minDistance(memo, word1, word2, i-1, j-1)
+
+	memo[i][j] = min(insert, delete, replace)
+
+	return memo[i][j]
 }
