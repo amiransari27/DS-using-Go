f23d4c29741f618e46a024ecaec8c507b72a79f3
Amir Ansari <amir_ansari50@yahoo.in>
2024-03-27 15:24:50 +0530

    Create countBits.go

---
+++ leetcode75/countBits.go
@@ -0,0 +1,17 @@
+package leetcode75
+
+func CountBits(n int) []int {
+	res := make([]int, n+1)
+
+	res[0] = 0
+
+	for i := 1; i <= n; i++ {
+		if i%2 == 0 {
+			res[i] = res[i/2]
+		} else {
+			res[i] = res[i/2] + 1
+		}
+	}
+
+	return res
+}
