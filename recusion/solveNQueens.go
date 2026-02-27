5e4a56f4b22f2fe40dc577f23481f38127932d56
amiransari27 <amir.amiransari987@gmail.com>
2026-02-27 11:23:02 +0530

    update nQueens

--- recusion/solveNQueens.go
+++ recusion/solveNQueens.go
@@ -7,7 +7,7 @@ package recusion
 func solveNQueens(n int) [][]string {
 	var res [][]string
 
-	// initialize empty board
+	//board
 	board := make([][]byte, n)
 	for i := range board {
 		board[i] = make([]byte, n)
@@ -15,41 +15,47 @@ func solveNQueens(n int) [][]string {
 			board[i][j] = '.'
 		}
 	}
+	//isvalid
+	var isValid func(int, int) bool
 
-	// check whether placing a queen at (row,col) is safe
-	var isValid func(row, col int) bool
 	isValid = func(row, col int) bool {
-		// column
+
+		// row
 		for i := 0; i < row; i++ {
 			if board[i][col] == 'Q' {
 				return false
 			}
 		}
+
 		// diagonal left-up
 		for i, j := row-1, col-1; i >= 0 && j >= 0; i, j = i-1, j-1 {
 			if board[i][j] == 'Q' {
 				return false
 			}
 		}
+
 		// diagonal right-up
 		for i, j := row-1, col+1; i >= 0 && j < n; i, j = i-1, j+1 {
 			if board[i][j] == 'Q' {
 				return false
 			}
 		}
+
 		return true
 	}
 
-	var dfs func(row int)
+	// dfs
+	var dfs func(int)
+
 	dfs = func(row int) {
-		if row == n {
-			// record current board as a solution
+
+		if row >= n {
 			sol := make([]string, n)
-			for i := 0; i < n; i++ {
+			for i := range board {
 				sol[i] = string(board[i])
 			}
+
 			res = append(res, sol)
-			return
 		}
 
 		for col := 0; col < n; col++ {
@@ -59,6 +65,7 @@ func solveNQueens(n int) [][]string {
 				board[row][col] = '.'
 			}
 		}
+
 	}
 
 	dfs(0)
