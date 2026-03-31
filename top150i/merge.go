9c7e9008b2c47c8c0befa62e2bb2364785208b19
amiransari27 <amir.amiransari987@gmail.com>
2026-03-31 16:42:06 +0530

    adding merge sort array

---
+++ top150i/merge.go
@@ -0,0 +1,20 @@
+package top150i
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+
+	p1, p2, i := m-1, n-1, m+n-1
+
+	for p2 >= 0 {
+
+		if p1 >= 0 && nums1[p1] > nums2[p2] {
+			nums1[i] = nums1[p1]
+			i--
+			p1--
+		} else {
+			nums1[i] = nums2[p2]
+			i--
+			p2--
+		}
+
+	}
+}
