6a7b3ffc27a583fa0dc4d2427abacc2b5ac89089
amiransari27 <amir.amiransari987@gmail.com>
2026-04-14 12:02:45 +0530

    adding strStr function to implement substring search in a given string

---
+++ top150i/strStr.go
@@ -0,0 +1,20 @@
+package top150i
+
+func strStr(haystack string, needle string) int {
+
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+
+		for j := 0; j < len(needle); j++ {
+
+			if haystack[i+j] != needle[j] {
+				break
+			}
+
+			if j == len(needle)-1 {
+				return i
+			}
+		}
+	}
+
+	return -1
+}
