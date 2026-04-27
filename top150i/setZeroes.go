a5e5b07962bac643845c4bad5d6a73d39d60a7a2
amiransari27 <amir.amiransari987@gmail.com>
2026-04-27 11:50:27 +0530

    add setZeroes function to set matrix rows and columns to zero based on zero elements

---
+++ top150i/setZeroes.go
@@ -0,0 +1,36 @@
+package top150i
+
+func setZeroes(matrix [][]int) {
+
+	m := len(matrix)
+	n := len(matrix[0])
+
+	res := make([][]int, m)
+	for i := range res {
+		res[i] = append([]int{}, matrix[i]...)
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if matrix[i][j] == 0 {
+
+				for k := 0; k < n; k++ {
+					res[i][k] = 0
+				}
+
+				for k := 0; k < m; k++ {
+					res[k][j] = 0
+				}
+
+			}
+		}
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+
+			matrix[i][j] = res[i][j]
+		}
+	}
+
+}
