2ce39d02864ef1a69753b11680cb77dca96863d5
amiransari27 <amir.amiransari987@gmail.com>
2026-04-28 16:07:06 +0530

    add setZeroes2 function to set matrix rows and columns to zero using boolean arrays

--- top150i/setZeroes.go
+++ top150i/setZeroes.go
@@ -34,3 +34,34 @@ func setZeroes(matrix [][]int) {
 	}
 
 }
+
+func setZeroes2(matrix [][]int) {
+
+	m := len(matrix)
+	n := len(matrix[0])
+
+	row := make([]bool, m)
+	col := make([]bool, n)
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+
+			if matrix[i][j] == 0 {
+				row[i] = true
+				col[j] = true
+			}
+
+		}
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+
+			if row[i] == true || col[j] == true {
+				matrix[i][j] = 0
+			}
+
+		}
+	}
+
+}
