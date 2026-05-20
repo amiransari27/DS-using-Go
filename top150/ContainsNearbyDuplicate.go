945e4dd5f41e04fbc99bcfb0f1480216aa300146
amiransari27 <amir.amiransari987@gmail.com>
2026-05-20 13:18:34 +0530

    refactor ContainsNearbyDuplicate function for improved readability

--- top150/ContainsNearbyDuplicate.go
+++ top150/ContainsNearbyDuplicate.go
@@ -1,27 +1,25 @@
 package top150
 
 func ContainsNearbyDuplicate(nums []int, k int) bool {
-   numSet := make(map[int]bool)
+	numSet := make(map[int]bool)
 
-   i, j := 0, 0
-   for j < len(nums) {
-       // check i-j
-       if j-i > k {
-           // shrink
-           delete(numSet, nums[i])
-           i++
-       }
+	i, j := 0, 0
+	for j < len(nums) {
+		// check i-j
+		if j-i > k {
+			// shrink
+			delete(numSet, nums[i])
+			i++
+		}
 
-       // check set
-       if _, exist := numSet[nums[j]]; exist {
-           return true
-       } else {
-           numSet[nums[j]] = true
-       }
-       j++
-   }
+		// check set
+		if _, exist := numSet[nums[j]]; exist {
+			return true
+		} else {
+			numSet[nums[j]] = true
+		}
+		j++
+	}
 
-   return false
+	return false
 }
-
-
